007379c88b857d5374aa44a3d658cf4531db654d
papasahti <atrepkin.pavel@gmail.com>
2026-04-09 21:57:16 +0300

    edit docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ if [ "${CANNONBALL_PRINT_BOOTSTRAP_CREDENTIALS:-false}" = "true" ]; then
   echo "========================================"
   echo "cannonball quick start"
   echo "Admin login: ${APP_USERNAME:-admin}"
-  echo "Admin password: ${APP_PASSWORD:-not-set}"
+  echo "Admin password: ${APP_PASSWORD:-adminadmin}"
   echo "Open URL: ${CANNONBALL_PUBLIC_URL:-http://127.0.0.1:${PORT:-8080}}"
   echo "========================================"
 fi
